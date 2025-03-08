2f918b05c662626e05499a51a99760a8a897625f
speedcode247 <speedcode247@gmail.com>
2025-03-08 10:06:11 +0700

    update footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -49,7 +49,7 @@ const Footer = () => {
 
       <div className="w-full  mt-16 md:mt-24 relative font-medium border-t border-solid border-light py-6 px-8 flex  flex-col md:flex-row items-center justify-between">
         <span className="text-center">
-          &copy;2023 TheGoatsFX. All rights reserved.
+          &copy;2025 TheGoatsFX. All rights reserved.
         </span>
         <Link
           href="/sitemap.xml"
