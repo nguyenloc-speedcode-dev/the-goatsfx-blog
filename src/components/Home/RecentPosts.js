655a68fa4620fbc5a868efc82c8034c2b0e182a7
speedcode247 <speedcode247@gmail.com>
2025-03-12 08:22:05 +0700

    update layout

--- src/components/Home/RecentPosts.js
+++ src/components/Home/RecentPosts.js
@@ -22,7 +22,7 @@ const RecentPosts = ({ blogs }) => {
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 grid-rows-2 gap-16 mt-16">
-        {filteredBlog.slice(0, 13).map((blog, index) => {
+        {filteredBlog.slice(0, 12).map((blog, index) => {
           return (
             <article key={index} className="col-span-1 row-span-1 relative">
               <BlogLayoutThree blog={blog} />
